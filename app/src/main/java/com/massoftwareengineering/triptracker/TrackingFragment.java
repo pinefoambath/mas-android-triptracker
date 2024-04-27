1b98cce51252c374f368545f0d45d992c99dede4
Andrea Kreideweiss <akreideweiss@gmail.com>
2024-04-27 10:45:54 +0200

    Change button look after click

--- app/src/main/java/com/massoftwareengineering/triptracker/TrackingFragment.java
+++ app/src/main/java/com/massoftwareengineering/triptracker/TrackingFragment.java
@@ -8,16 +8,19 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class TrackingFragment extends Fragment {
+
+    private Button startTrackingButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tracking, container, false);
 
-        Button startTrackingButton = rootView.findViewById(R.id.startButton);
+        startTrackingButton = rootView.findViewById(R.id.startButton);
 
         startTrackingButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +33,9 @@ public class TrackingFragment extends Fragment {
     }
 
     private void startTracking() {
-        // Todo: start the tracking
-        // Todo: update the UI to show tracking
+        // TODO: actually start the GPS tracking functionality
+
+        startTrackingButton.setText(R.string.tracking);
+        startTrackingButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.tracking_active));
     }
 }
