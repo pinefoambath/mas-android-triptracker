737ba67a219fef2502d2e129565c91147c3bfd1c
Andrea Kreideweiss <akreideweiss@gmail.com>
2024-04-28 10:18:33 +0200

    Allow to create and submit a trip note

--- app/src/main/java/com/massoftwareengineering/triptracker/TrackingFragment.java
+++ app/src/main/java/com/massoftwareengineering/triptracker/TrackingFragment.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,70 +14,73 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-
 public class TrackingFragment extends Fragment {
 
-    private Button startTrackingButton;
-    private LinearLayout tripFinishedForm;
+    private Button startTrackingButton, submitButton;
     private EditText tripNotes;
+    private TextView welcomeText, formInstructions;
     private boolean isTracking = false;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tracking, container, false);
+
         startTrackingButton = rootView.findViewById(R.id.startButton);
-        startTrackingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        submitButton = rootView.findViewById(R.id.submitButton);
+        welcomeText = rootView.findViewById(R.id.welcomeText);
+        formInstructions = rootView.findViewById(R.id.formInstructions);
+        tripNotes = rootView.findViewById(R.id.tripNotes);
+
+        startTrackingButton.setOnClickListener(v -> {
+            if (!isTracking) {
                 startTracking();
+            } else {
+                finishTracking();
             }
         });
+
+        submitButton.setOnClickListener(v -> submitTrip());
 
         return rootView;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        tripFinishedForm = view.findViewById(R.id.tripFinishedForm);
-        tripNotes = view.findViewById(R.id.tripNotes);
-        startTrackingButton = view.findViewById(R.id.startButton);
-
-        startTrackingButton.setOnClickListener(v -> {
-            if (isTracking) {
-                finishTracking();
-            } else {
-                startTracking();
-            }
-        });
-
-        view.findViewById(R.id.submitButton).setOnClickListener(v -> submitTrip());
-    }
-
     private void startTracking() {
-        // todo: actually start the gps service
+        // TODO: start GPS tracking service
         isTracking = true;
-        startTrackingButton.setText(R.string.tracking);
+        startTrackingButton.setText(R.string.stop_tracking);
         startTrackingButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.tracking_active));
-    }
 
+        welcomeText.setVisibility(View.GONE);
+        formInstructions.setVisibility(View.GONE);
+        tripNotes.setVisibility(View.GONE);
+        submitButton.setVisibility(View.GONE);
+    }
 
     private void finishTracking() {
+        // TODO: stop GPS tracking service
         isTracking = false;
+
         startTrackingButton.setText(R.string.start_tracking);
         startTrackingButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.default_button));
 
-        tripFinishedForm.setVisibility(View.VISIBLE);
+        formInstructions.setVisibility(View.VISIBLE);
+        tripNotes.setVisibility(View.VISIBLE);
+        submitButton.setVisibility(View.VISIBLE);
         Toast.makeText(requireContext(), "Trip Finished", Toast.LENGTH_SHORT).show();
     }
 
     private void submitTrip() {
-        String notes = tripNotes.getText().toString();
-        // TODO: Handle trip submission (post call to backend.)
+        // TODO: make POST call to .NET backend with track and notes data
 
-        tripFinishedForm.setVisibility(View.GONE);
         tripNotes.setText("");
+        formInstructions.setVisibility(View.GONE);
+        tripNotes.setVisibility(View.GONE);
+        submitButton.setVisibility(View.GONE);
+        welcomeText.setVisibility(View.VISIBLE);
+        startTrackingButton.setVisibility(View.VISIBLE);
+
+        Toast.makeText(requireContext(), getString(R.string.trip_submitted), Toast.LENGTH_SHORT).show();
+
     }
 }
