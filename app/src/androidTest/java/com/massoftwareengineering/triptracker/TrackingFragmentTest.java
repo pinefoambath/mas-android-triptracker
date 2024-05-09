a452e0f5c4831e6748515775ee5640e7197edda6
Andrea Kreideweiss <48927644+pinefoambath@users.noreply.github.com>
2024-05-09 14:02:27 +0200

    Make button and form field to start, finish and submit trip (#6)
    **Feature changes:**
    - Allows to "start a trip" via the click of the button. At the moment
    that does nothing but change the color of the button and show a
    different button text. In the future this is where we would start the
    GPS tracking from.
    - Allows to stop the trip. 
    - After stopping the trip, shows a text field for entering some trip
    notes. These can then be "submitted" by the click of a button. Nothing
    happens here yet, but it is the base for building the trip object with
    the gps and notes data, and sending it via POST call to the .NET
    backend.
    - The view then changes back to the start view. 
    
    - Adds a test: I am aware this uses a deprecated method, but i wasn't
    able to make this work otherwies. I added a refactoring ticket to the
    backlog for android Trello bug card
    https://trello.com/c/XuNCEJFS/57-android-bug-backlog
    
    **DevOps changes:**
    Adds a test runner for Github. It will now run all tests when a new PR
    is made, to any branch.

---
+++ app/src/androidTest/java/com/massoftwareengineering/triptracker/TrackingFragmentTest.java
@@ -0,0 +1,39 @@
+package com.massoftwareengineering.triptracker;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class TrackingFragmentTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void startAndStopTracking() {
+        onView(withId(R.id.startButton)).check(matches(isDisplayed()));
+        onView(withId(R.id.startButton)).check(matches(withText(R.string.start_tracking)));
+        onView(withId(R.id.formInstructions)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.startButton)).perform(click());
+        onView(withId(R.id.startButton)).check(matches(withText(R.string.stop_tracking)));
+        onView(withId(R.id.formInstructions)).check(matches(not(isDisplayed())));
+
+        onView(withId(R.id.startButton)).perform(click());
+        onView(withId(R.id.startButton)).check(matches(withText(R.string.start_tracking)));
+        onView(withId(R.id.formInstructions)).check(matches(isDisplayed()));
+    }
+}
--- app/src/main/java/com/massoftwareengineering/triptracker/TrackingFragment.java
+++ app/src/main/java/com/massoftwareengineering/triptracker/TrackingFragment.java
@@ -5,32 +5,82 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class TrackingFragment extends Fragment {
+
+    private Button startTrackingButton, submitButton;
+    private EditText tripNotes;
+    private TextView welcomeText, formInstructions;
+    private boolean isTracking = false;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tracking, container, false);
 
-        Button startTrackingButton = rootView.findViewById(R.id.startButton);
+        startTrackingButton = rootView.findViewById(R.id.startButton);
+        submitButton = rootView.findViewById(R.id.submitButton);
+        welcomeText = rootView.findViewById(R.id.welcomeText);
+        formInstructions = rootView.findViewById(R.id.formInstructions);
+        tripNotes = rootView.findViewById(R.id.tripNotes);
 
-        startTrackingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        startTrackingButton.setOnClickListener(v -> {
+            if (!isTracking) {
                 startTracking();
+            } else {
+                finishTracking();
             }
         });
+
+        submitButton.setOnClickListener(v -> submitTrip());
 
         return rootView;
     }
 
     private void startTracking() {
-        // Todo: start the tracking
-        // Todo: update the UI to show tracking
+        // TODO: start GPS tracking service
+        isTracking = true;
+        startTrackingButton.setText(R.string.stop_tracking);
+        startTrackingButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.tracking_active));
+
+        welcomeText.setVisibility(View.GONE);
+        formInstructions.setVisibility(View.GONE);
+        tripNotes.setVisibility(View.GONE);
+        submitButton.setVisibility(View.GONE);
+    }
+
+    private void finishTracking() {
+        // TODO: stop GPS tracking service
+        isTracking = false;
+
+        startTrackingButton.setText(R.string.start_tracking);
+        startTrackingButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.default_button));
+
+        formInstructions.setVisibility(View.VISIBLE);
+        tripNotes.setVisibility(View.VISIBLE);
+        submitButton.setVisibility(View.VISIBLE);
+        Toast.makeText(requireContext(), getString(R.string.trip_finished), Toast.LENGTH_SHORT).show();
+    }
+
+    private void submitTrip() {
+        // TODO: make POST call to .NET backend with track and notes data
+
+        tripNotes.setText("");
+        formInstructions.setVisibility(View.GONE);
+        tripNotes.setVisibility(View.GONE);
+        submitButton.setVisibility(View.GONE);
+        welcomeText.setVisibility(View.VISIBLE);
+        startTrackingButton.setVisibility(View.VISIBLE);
+
+        Toast.makeText(requireContext(), getString(R.string.trip_submitted), Toast.LENGTH_SHORT).show();
+
     }
 }
