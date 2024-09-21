62f3af0587f804d2872d2f97e9b2a1122a746b57
Andrea Kreideweiss <akreideweiss@gmail.com>
2024-09-21 18:45:06 +0200

    Extend connection timeouts for http client

--- app/src/main/java/com/massoftwareengineering/triptracker/data/network/RetrofitClient.java
+++ app/src/main/java/com/massoftwareengineering/triptracker/data/network/RetrofitClient.java
@@ -2,6 +2,9 @@ package com.massoftwareengineering.triptracker.data.network;
 
 import com.massoftwareengineering.triptracker.data.repository.TripService;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,8 +14,15 @@ public class RetrofitClient {
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(60, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .writeTimeout(60, TimeUnit.SECONDS)
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
